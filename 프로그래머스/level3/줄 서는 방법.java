b84a6481471d89845ca017513b2418de152882a6
TotheMoon12 <46846455+TotheMoon12@users.noreply.github.com>
2022-04-29 13:29:31 +0900

    Create 줄 서는 방법.java

---
+++ 프로그래머스/level3/줄 서는 방법.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int n, long k) {
+        int[] answer = new int[n];
+        final int FACTORIAL_MAX = 20;
+
+        ArrayList<Integer> persons = new ArrayList<>();
+        for (int i = 1; i <= n; ++i) {
+            persons.add(i);
+        }
+
+        long[] factorials = new long[FACTORIAL_MAX];
+        factorials[0] = 1;
+        factorials[1] = 1;
+
+
+        for (int i = 2; i < FACTORIAL_MAX; ++i) {
+            factorials[i] = factorials[i - 1] * i;
+        }
+
+        final int PERSON = n;
+        --n;
+        int index = 0;
+        while (index < PERSON - 1) {
+            int order = (int) (k / factorials[n]);
+            if (k % factorials[n] == 0) {
+                --order;
+                k -= Math.max(0, order) * factorials[n];
+            } else {
+                k %= factorials[n];
+            }
+
+            answer[index++] = persons.get(order);
+            persons.remove(order);
+            --n;
+        }
+
+        answer[index] = persons.get(0);
+        return answer;
+    }
+}
