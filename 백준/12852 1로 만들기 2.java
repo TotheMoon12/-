07288cbf8f5b98aba96c78334818be3cddbd4aa4
TotheMoon12 <46846455+TotheMoon12@users.noreply.github.com>
2022-05-09 12:02:09 +0900

    Create 12852 1로 만들기 2.java

---
+++ 백준/12852 1로 만들기 2.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[N + 1][2];
+        for (int i = 1; i<= N; ++i) {
+            dp[i][0] = -1;
+        }
+
+        final int COUNT = 0;
+        final int PREV = 1;
+        dp[N][0] = 0;
+        for (int i = N; i >= 2; --i) {
+            if (dp[i][COUNT] != -1) {
+                if (i % 3 == 0) {
+                    int next = i / 3;
+                    if (dp[next][COUNT] == -1) {
+                        dp[next][COUNT] = dp[i][COUNT] + 1;
+                        dp[next][PREV] = i;
+                    } else {
+                        if (dp[i][COUNT] + 1 < dp[next][COUNT]) {
+                            dp[next][COUNT] = dp[i][COUNT] + 1;
+                            dp[next][PREV] = i;
+                        }
+                    }
+                }
+
+                if (i % 2 == 0) {
+                    int next = i / 2;
+                    if (dp[next][COUNT] == -1) {
+                        dp[next][COUNT] = dp[i][COUNT] + 1;
+                        dp[next][PREV] = i;
+                    } else {
+                        if (dp[i][COUNT] + 1 < dp[next][COUNT]) {
+                            dp[next][COUNT] = dp[i][COUNT] + 1;
+                            dp[next][PREV] = i;
+                        }
+                    }
+                }
+
+                int next = i - 1;
+                if (dp[next][COUNT] == -1) {
+                    dp[next][COUNT] = dp[i][COUNT] + 1;
+                    dp[next][PREV] = i;
+                } else {
+                    if (dp[i][COUNT] + 1 < dp[next][COUNT]) {
+                        dp[next][COUNT] = dp[i][COUNT] + 1;
+                        dp[next][PREV] = i;
+                    }
+                }
+            }
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        int start = 1;
+        while (start < N) {
+            list.add(start);
+
+            start = dp[start][PREV];
+        }
+
+        System.out.println(dp[1][COUNT]);
+        StringBuilder sb = new StringBuilder();
+        sb.append(N);
+        sb.append(' ');
+        for (int i = list.size() - 1; i >= 0; --i) {
+            sb.append(list.get(i));
+            sb.append(' ');
+        }
+
+        System.out.println(sb.toString());
+    }
+}
