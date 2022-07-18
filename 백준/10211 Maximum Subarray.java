b83a17d97a43d374c4ed4ae0b27ff9dfc181553d
TotheMoon12 <46846455+TotheMoon12@users.noreply.github.com>
2022-07-18 11:06:32 +0900

    Create 10211 Maximum Subarray.java

---
+++ 백준/10211 Maximum Subarray.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int t = 0; t < T; ++t) {
+            int N = Integer.parseInt(br.readLine());
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int answer = Integer.parseInt(st.nextToken());
+            int sum = answer;
+            for (int n = 1; n < N; ++n) {
+                int number = Integer.parseInt(st.nextToken());
+
+                if (sum + number > number) {
+                    sum += number;
+                } else {
+                    sum = number;
+                }
+
+                answer = Math.max(answer, sum);
+            }
+
+            sb.append(answer);
+            sb.append(System.lineSeparator());
+        }
+
+        System.out.print(sb);
+    }
+}
