3c7deca1a003f21abc576834b5a5ed11d72fc6be
TotheMoon12 <46846455+TotheMoon12@users.noreply.github.com>
2022-07-26 15:27:41 +0900

    Create 13305 주유소.java

---
+++ 백준/13305 주유소.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer stDistance = new StringTokenizer(br.readLine());
+        StringTokenizer stCost = new StringTokenizer(br.readLine());
+
+        int[] distances = new int[N - 1];
+        int[] costsByCity = new int[N - 1];
+        for (int idx = 0; idx < N - 1; ++idx) {
+            distances[idx] = Integer.parseInt(stDistance.nextToken());
+            costsByCity[idx] = Integer.parseInt(stCost.nextToken());
+        }
+
+        int cost = costsByCity[0];
+        long answer = 0;
+
+        for (int n = 1; n < N; ++n) {
+            answer += (long)(distances[n - 1]) * cost;
+            if (costsByCity[n] < cost) {
+                cost = costsByCity[n];
+            }
+        }
+
+        System.out.print(answer);
+    }
+}
